9399f99b2b67df2c8326b904434d6703a1d62792
Juan Ignacio <juani8.bustos@gmail.com>
2025-06-06 12:52:10 -0300

    Arreglado Cube

--- Cube.cs
+++ Cube.cs
@@ -4,7 +4,7 @@ using System;
 public partial class Cube : MeshInstance3D
 {
 	public override void _Process(double delta)
-	{ //aaaa
-		Translate(Vector3.Up ** (float)delta);
+	{
+		Translate(Vector3.Up * (float)delta);
 	}
 }
