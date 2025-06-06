12897db986d327781f144e95be0afdad5110094d
octokerbs <kerbsod@gmail.com>
2025-06-06 15:54:22 -0300

    Character bones can be modified

--- CharacterMover.cs
+++ CharacterMover.cs
@@ -4,6 +4,35 @@ public partial class CharacterMover : CharacterBody3D
 {
     [Export] public float Speed = 3.0f;
 
+    private Skeleton3D skeleton;
+
+    public override void _Ready()
+    {
+        skeleton = GetNode<Skeleton3D>("Character/Skeleton3D");
+
+        int boneCount = skeleton.GetBoneCount();
+        GD.Print("Total bones: " + boneCount);
+
+        for (int i = 0; i < boneCount; i++)
+        {
+            string boneName = skeleton.GetBoneName(i);
+            GD.Print($"Bone {i}: {boneName}");
+        }
+
+        // Example: rotate a known bone by name
+        int spineIndex = skeleton.FindBone("mixamorig_Spine");
+        if (spineIndex != -1)
+        {
+            var pose = skeleton.GetBonePose(spineIndex);
+            pose.Basis = pose.Basis.Rotated(Vector3.Right, Mathf.DegToRad(100));
+            skeleton.SetBonePose(spineIndex, pose);
+        }
+        else
+        {
+            GD.Print("Spine bone not found.");
+        }
+    }
+
     public override void _PhysicsProcess(double delta)
     {
         Vector3 velocity = Velocity;
